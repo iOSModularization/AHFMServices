e98b9d2ea6cb8d4e2d9acd74cede1190f16c9d36
iOSModularization <Hurricane@Awesome-Sht.local>
2017-09-29 13:03:59 -0700

    Add default properties

--- AHFMServices/Classes/AHFMServices.swift
+++ AHFMServices/Classes/AHFMServices.swift
@@ -1,5 +1,28 @@
-
+import Foundation
 
 public protocol AHFMServices {
+    
     static func register()
+    
+}
+public extension AHFMServices {
+    /// The service name
+    public static var service: String {
+        return "\(Self.self)"
+    }
+    
+    /// To navigate to a VC
+    public static var taskNavigation: String {
+        return "taskNavigation"
+    }
+    
+    /// To create a VC and return it to the caller
+    public static var taskCreateVC: String {
+        return "taskCreateVC"
+    }
+    
+    /// The key to extract the VC from a userInfo dictionary
+    public static var keyGetVC: String {
+        return "keyGetVC"
+    }
 }
